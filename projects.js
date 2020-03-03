821b8e7b3fc88b93a6c89d67d5c479b21a1efd9a
dseng905 <35271159+dseng905@users.noreply.github.com>
2020-03-02 20:52:34 -0800

    Added Descriptions for Projects
    -Added mouse-over events on project tiles to display description and tools used

--- projects.js
+++ projects.js
@@ -1,19 +1,46 @@
 
-const ProjectTile = (props) => (
-  <div >
-    <a className="project-link" target="_blank" href={props.url}>
-      <div 
-        className="project-tile"
-        style={{
-          backgroundSize: 'cover', 
-          backgroundImage: 'url(' + props.img + ')'
-        }}
-      >
-        <p className="project-name">{props.name}</p>
+class ProjectTile extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      mouseOver: false
+    }
+  }
+
+  render(){
+    return (
+      <div>
+        <a className="project-link" target="_blank" href={this.props.url}>
+          <div 
+            className="project-tile"
+            style={{
+              backgroundSize: 'cover', 
+              backgroundImage: 'url(' + this.props.img + ')'
+            }}
+            onMouseOver={() => this.setState({mouseOver: true})}
+            onMouseLeave={() => this.setState({mouseOver: false})}
+          >
+            <div className="project-text">
+              {
+                this.state.mouseOver ? 
+                (
+                  <div>
+                    <p className="project-desc">{this.props.desc}</p>
+                    <div className="tech-list">
+                      {this.props.tech.map(item => (<p className="tech"><nobr>{item}</nobr></p>))}
+                    </div>
+                  </div>
+                ) :
+                (<p className="project-name">{this.props.name}</p>)
+              }
+            </div>
+          </div>
+        </a>
       </div>
-    </a>
-  </div>
-)
+    )
+  }
+}
+ 
 
 const ProjectSection = (props) => (
   <div>
@@ -21,7 +48,7 @@ const ProjectSection = (props) => (
     <div className="project-tiles">
     {
       props.projects.map(obj => (
-        <ProjectTile name={obj.name} img={obj.img} url={obj.url}/>
+        <ProjectTile name={obj.name} img={obj.img} url={obj.url} desc={obj.desc} tech={obj.tech}/>
       ))
     }
     </div>
